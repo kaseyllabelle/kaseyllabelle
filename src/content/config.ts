16fbb00446f3b7f0dd878e43f4654ca2a466e574
Kasey <kaseyllabelle@Kaseys-MacBook-Pro-2.local>
2024-10-27 22:09:46 -0400

    update content schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { z, defineCollection } from 'astro:content';
 const projectsCollection = defineCollection({
   type: 'content',
   schema: z.object({
-    sortOrder: z.number(),
     tag: z.string().optional(),
     title: z.string(),
     description: z.string(),
